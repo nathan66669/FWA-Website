09c3458ff94e51a61bb11056bf643865d7de69e4
NateDwag <nathan66669@gmail.com>
2018-04-15 13:54:58 -0400

    rem p

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       ref: 'origin/master',
       key:  '~/.ssh/theFWA.pem',   
       repo: 'git@github.com:nathan66669/FWA-Website.git',
-      path: '/home/ubuntu/FWAp',
+      path: '/home/ubuntu/FWA',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
