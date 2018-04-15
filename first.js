2f003554300bde69f5cc7e3150f27c87d1b9f309
NateDwag <nathan66669@gmail.com>
2018-04-15 17:05:47 -0400

    y

--- first.js
+++ first.js
@@ -1,11 +1,12 @@
 //my first test JS
-import clashApi from 'clash-of-clans-api';
-let    client = clashApi({
-  token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijg0YmY4MGE1LTY4OTQtNGU3ZC04YTI3LWY4OWQ3ZDE1ODgyNSIsImlhdCI6MTUyMzQ2MDQxNSwic3ViIjoiZGV2ZWxvcGVyLzAzMWQwZDgwLThiOWUtZjdmNS02OWVkLTIzYzUxZGIzMTNkYyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE4LjIxNy4xMjIuMTYxIl0sInR5cGUiOiJjbGllbnQifV19.bppIhQjKVfU2rv7JiZH0RvXDxNpNumQer2AIynLnIFJrJZFPocXzZcmG6o0XVhZakK86qYg6nVviYyEfpZqifg"
-});
+
+        import clashApi from '../node_modules/clash-of-clans-api';
+        let client = clashApi({
+            token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijg0YmY4MGE1LTY4OTQtNGU3ZC04YTI3LWY4OWQ3ZDE1ODgyNSIsImlhdCI6MTUyMzQ2MDQxNSwic3ViIjoiZGV2ZWxvcGVyLzAzMWQwZDgwLThiOWUtZjdmNS02OWVkLTIzYzUxZGIzMTNkYyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE4LjIxNy4xMjIuMTYxIl0sInR5cGUiOiJjbGllbnQifV19.bppIhQjKVfU2rv7JiZH0RvXDxNpNumQer2AIynLnIFJrJZFPocXzZcmG6o0XVhZakK86qYg6nVviYyEfpZqifg"
+        });
 
 
-client
-  .clanByTag('#29YR22QY')
-  .then(response => document.getElementById("length").innerHTML  = response.items.length)
-  .catch(err => console.log(err));
+        client
+            .clanByTag('#29YR22QY')
+            .then(response => document.getElementById("length").innerHTML = response.items.length)
+            .catch(err => console.log(err));
