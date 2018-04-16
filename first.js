9f660c286098c6afa93ed78bcda96e756c65b7bc
NateDwag <nathan66669@gmail.com>
2018-04-15 20:48:12 -0400

    v

--- first.js
+++ first.js
@@ -1,13 +1,13 @@
 //my first test JS
 
 // import clashApi from 'node_modules/clash-of-clans-api';
-import clashApi from "clash-of-clans-api";
-let client = clashApi({
+const clashApi = require('clash-of-clans-api');
+var client = clashApi({
     token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijg0YmY4MGE1LTY4OTQtNGU3ZC04YTI3LWY4OWQ3ZDE1ODgyNSIsImlhdCI6MTUyMzQ2MDQxNSwic3ViIjoiZGV2ZWxvcGVyLzAzMWQwZDgwLThiOWUtZjdmNS02OWVkLTIzYzUxZGIzMTNkYyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE4LjIxNy4xMjIuMTYxIl0sInR5cGUiOiJjbGllbnQifV19.bppIhQjKVfU2rv7JiZH0RvXDxNpNumQer2AIynLnIFJrJZFPocXzZcmG6o0XVhZakK86qYg6nVviYyEfpZqifg"
 });
 
 
 client
     .clanByTag('#29YR22QY')
-    .then(response => document.getElementById("length").innerHTML = response.items.length)
+    .then(response => console.log(response))
     .catch(err => console.log(err));
