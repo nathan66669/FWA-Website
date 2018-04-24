3ac742f877bded2c0aa6f9e7709f1d7ca29eb824
nathan <37780747+nathan66669@users.noreply.github.com>
2018-04-24 14:14:28 -0400

    test

--- fbconfig.js
+++ fbconfig.js
@@ -2,5 +2,6 @@ var firebase = require("firebase");
 var config = require('config');
 
 firebase.initializeApp(config.get('firebaseConfiguration'));
+
 var database = firebase.database();
 module.exports.database = database;
