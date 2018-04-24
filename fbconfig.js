e94740f547203fe51787335c06a8d4239e5da1a2
nathan <37780747+nathan66669@users.noreply.github.com>
2018-04-24 13:27:02 -0400

    test

--- fbconfig.js
+++ fbconfig.js
@@ -1,6 +1,7 @@
 var firebase = require("firebase");
+const config = require('config');
 
-firebase.initializeApp(config.get(fbConfig));
+firebase.initializeApp(config.get('fbConfig'));
 var database = firebase.database();
 var playersRef = database.ref("players");
 var clansRef = database.ref("clans");
