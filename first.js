5fd369d5bb2c1bde0f525e842e85182754a578c5
nathan <37780747+nathan66669@users.noreply.github.com>
2018-04-24 14:23:50 -0400

    testing

--- first.js
+++ first.js
@@ -5,7 +5,7 @@ var clashApi = require('clash-of-clans-api');
 var APIclient = clashApi(config.get('clashToken'));
 var playersRef = require('./fbconfig.js')
 //var playersRef = database.ref("players");
-//var clansRef = database.ref("clans");
+var clansRef = require('./fbconfig.js')
 //Redis
 var redis2 = require('ioredis');
 var redis = redis2.createClient();
