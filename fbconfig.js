84457e9b9322d5a5e889247334f4b02fd14b6f52
nathan <37780747+nathan66669@users.noreply.github.com>
2018-04-24 13:58:02 -0400

    test

--- fbconfig.js
+++ fbconfig.js
@@ -1,5 +1,5 @@
 var firebase = require("firebase");
 var config = require('config');
 
-firebase.initializeApp(config.get('fbToken'));
+firebase.initializeApp(config.get('firebaseConfiguration'));
 var database = firebase.database();
